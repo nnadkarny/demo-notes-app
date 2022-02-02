3c1dcae20239d63fe8067d7f0425757b8e6946bf
nnadkarny <nnadkarny@gmail.com>
2022-02-02 10:36:41 -0500

    Removed frontend

--- stacks/index.js
+++ stacks/index.js
@@ -1,8 +1,14 @@
+import { RemovalPolicy } from "aws-cdk-lib";
 import StorageStack from "./StorageStack";
 import ApiStack from "./ApiStack";
 import AuthStack from "./AuthStack";
 
 export default function main(app) {
+  // Remove all resources when the dev stage is removed
+  if (app.stage === "dev") {
+    app.setDefaultRemovalPolicy(RemovalPolicy.DESTROY);
+  }
+
   const storageStack = new StorageStack(app, "storage");
 
   const apiStack = new ApiStack(app, "api", {
